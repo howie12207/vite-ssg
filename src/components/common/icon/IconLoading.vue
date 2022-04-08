b07f45daef6b43ac5c2ccc4cc9937f64ceb328fa
Howie <howie12207@gmail.com>
2022-04-08 14:47:10 +0800

    feat: add pure loading css

---
+++ src/components/common/icon/IconLoading.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    size: {
+        type: Number,
+        default: 80
+    },
+    mainColor: {
+        type: String,
+        default: 'currentColor'
+    }
+});
+const style = computed(() => {
+    return {
+        '--loading-size': `${props.size}px`,
+        '--loading-color': props.mainColor
+    };
+});
+</script>
+
+<template>
+    <div :style="style" class="icon_loading">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<style scoped>
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
+.icon_loading {
+    display: inline-block;
+    position: relative;
+    width: var(--loading-size);
+    height: var(--loading-size);
+}
+
+.icon_loading div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: calc(var(--loading-size) - 16px);
+    height: calc(var(--loading-size) - 16px);
+    margin: 8px;
+    border: 8px solid var(--loading-color);
+    border-radius: 50%;
+    animation: rotate 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: var(--loading-color) transparent transparent transparent;
+}
+
+.icon_loading div:nth-child(1) {
+    animation-delay: -0.45s;
+}
+
+.icon_loading div:nth-child(2) {
+    animation-delay: -0.3s;
+}
+
+.icon_loading div:nth-child(3) {
+    animation-delay: -0.15s;
+}
+
+@keyframes rotate {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- src/components/common/pageLoading/PageLoading.vue
+++ src/components/common/pageLoading/PageLoading.vue
@@ -1,33 +1,23 @@
+<script setup>
+import IconLoading from '@c/common/icon/IconLoading.vue';
+</script>
+
 <template>
-    <div class="loading"><img src="./img/loading.png" /></div>
+    <IconLoading class="loading" main-color="#004790" />
 </template>
 <style lang="scss" scoped>
 .loading {
     position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     top: 0;
+    bottom: 0;
     left: 0;
-    z-index: 10000;
+    right: 0;
+    z-index: 99999;
     width: 100vw;
     height: 100vh;
     background-color: rgb(0 0 0 / 80%);
-    text-align: center;
-    line-height: 100vh;
-    vertical-align: middle;
-
-    img {
-        display: inline-block;
-        animation: turn 1s ease infinite;
-        width: 80px;
-        height: 80px;
-    }
-}
-@keyframes turn {
-    from {
-        transform: rotate(0deg);
-    }
-
-    to {
-        transform: rotate(360deg);
-    }
 }
 </style>
